91bf1a3e7f3fdc0ad5f50a4edc679d842e90c04e
Vlanker <kinvlanknik@yandex.ru>
2022-02-14 00:48:10 +0300

    Binary object as bytes/string

---
+++ BaseToStringAndBack/Program.cs
@@ -0,0 +1,20 @@
+﻿byte[] binaryObject = new byte[128];
+
+new Random().NextBytes(binaryObject);
+
+Console.WriteLine("Binary object as bytes:");
+
+for(var index = 0; index < binaryObject.Length; index++)
+{
+    Console.Write($"{binaryObject[index]:X} ");
+}
+
+// foreach (var t in binaryObject)
+// {
+//     Console.Write($"{t:X}");
+// }
+
+Console.WriteLine();
+
+string encoded = Convert.ToBase64String(binaryObject);
+Console.WriteLine($"Binary object as string: {encoded}");
