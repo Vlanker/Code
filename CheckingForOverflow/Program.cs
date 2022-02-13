2dbe23c21d0c22acaa570fa3301a2be081dbb73a
Vlanker <kinvlanknik@yandex.ru>
2022-02-14 00:56:33 +0300

    Checking for overflow

---
+++ CheckingForOverflow/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+
+int x = int.MaxValue - 1;
+WriteLine($"Initial value: {x}");
+x++;
+WriteLine($"After incrementing: {x}");
+x++;
+WriteLine($"After incrementing: {x}");
+x++;
+WriteLine($"After incrementing: {x}");
+
+WriteLine();
+
+checked
+{
+    int y = int.MaxValue - 1;
+    WriteLine($"Initial value: {y}");
+    y++;
+    WriteLine($"After incrementing: {y}");
+    y++;
+    WriteLine($"After incrementing: {y}"); // System.OverflowException
+    y++;
+    WriteLine($"After incrementing: {y}");
+}
