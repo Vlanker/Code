19a48a2d26bf8c18e6a7910998cfa0ebcbcb7a9e
Vlanker <kinvlanknik@yandex.ru>
2022-02-16 02:03:33 +0300

    Instrumenting Debug and Trace with Microsoft.Extensions.Configuration

--- Instrumenting/Program.cs
+++ Instrumenting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
 
 namespace Instrumenting;
 
@@ -6,16 +7,33 @@ internal static class Program
 {
     // dotnet run --configuration Release
     // dotnet run --configuration Debug
-    private static void Main()
+    private static void Main(string[] args)
     {
         // запись в текстовый файл, расположенный в файле проекта
-         Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
-         // модуль записи текста буферизируется, поэтому данная опция 
-         // вызывает функцию Flush() для всех прослушивателей после записи
-         Trace.AutoFlush = true;
-        
+        Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+        // модуль записи текста буферизируется, поэтому данная опция 
+        // вызывает функцию Flush() для всех прослушивателей после записи
+        Trace.AutoFlush = true;
+
         Debug.WriteLine("Debug says, I am watching!");
         Trace.WriteLine("Trace says, I am watching!");
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json",
+                optional: true, reloadOnChange: true);
+
+        IConfigurationRoot configuration = builder.Build();
+
+        var ts = new TraceSwitch(
+            displayName: "PacktSwitch",
+            description: "This switch is set via a JSON config.");
+
+        configuration.GetSection("PacktSwitch").Bind(ts);
+
+        Trace.WriteLineIf(ts.TraceError, "Trace error");
+        Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+        Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+        Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
     }
 }
-
