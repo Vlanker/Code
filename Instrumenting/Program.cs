5d2c22806be58736678e5b96952b2d394830553c
Vlanker <kinvlanknik@yandex.ru>
2022-02-16 01:30:36 +0300

    Instrumenting Debug and Trace

---
+++ Instrumenting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace Instrumenting;
+
+internal static class Program
+{
+    // dotnet run --configuration Release
+    // dotnet run --configuration Debug
+    private static void Main()
+    {
+        // запись в текстовый файл, расположенный в файле проекта
+         Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+         // модуль записи текста буферизируется, поэтому данная опция 
+         // вызывает функцию Flush() для всех прослушивателей после записи
+         Trace.AutoFlush = true;
+        
+        Debug.WriteLine("Debug says, I am watching!");
+        Trace.WriteLine("Trace says, I am watching!");
+    }
+}
+
