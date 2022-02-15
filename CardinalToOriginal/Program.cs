261fde9e8d73c6544aa8ce60e7e0b689ce9ba222
Vlanker <kinvlanknik@yandex.ru>
2022-02-16 00:07:24 +0300

    Cardinal to original

---
+++ CardinalToOriginal/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+RunCardinalToOriginal();
+
+static string CardinalToOriginal(int number)
+{
+    switch (number)
+    {
+        case 11:
+        case 12:
+        case 13:
+            return $"{number}th";
+        default:
+            int lastDigit = number % 10;
+
+            string suffinx = lastDigit switch
+            {
+                1 => "st",
+                2 => "nd",
+                3 => "rd",
+                _ => "th"
+            };
+
+            return $"{number}{suffinx}";
+    }
+}
+
+static void RunCardinalToOriginal()
+{
+    for (int number = 1; number <= 40; number++)
+    {
+        Write($"{CardinalToOriginal(number)} ");
+    }
+
+    WriteLine();
+}
