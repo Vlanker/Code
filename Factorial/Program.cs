2152e0ea4153036344c53f20ee4f4ab68494bb80
Vlanker <kinvlanknik@yandex.ru>
2022-02-16 00:07:57 +0300

    Factorial recursive

---
+++ Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿RunFactorial();
+
+static int Factorial(int number)
+{
+    // if (number < 1)
+    // {
+    //     return 0;
+    // }
+    //
+    // if (number == 1)
+    // {
+    //     return 1;
+    // }
+    //
+    // return number * Factorial(number - 1);
+    
+    return number switch
+    {
+        < 1 => 0,
+        1 => 1,
+        _ => number * Factorial(number - 1)
+    };
+}
+
+static void RunFactorial()
+{
+    for (int i = 1; i < 15; i++)
+    {
+        Console.WriteLine($"{i}! =  {Factorial(i):N0}");
+    }
+}
