723a1c3d482423e230707e40c88ce0168dda54f0
Vlanker <kinvlanknik@yandex.ru>
2022-02-18 01:23:46 +0300

    Factorial on local function

---
+++ FactorialLocalFunction/Program.cs
@@ -0,0 +1,20 @@
+﻿var number = 10;
+Console.WriteLine($"{number}! = {Factorial(number):N0}");
+
+static int Factorial(int number)
+{
+    if (number < 0)
+    {
+        throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+    }
+
+    return LocalFactorial(number);
+
+    int LocalFactorial(int localNumber)
+    {
+        if (localNumber < 1)
+            return 1;
+
+        return localNumber * LocalFactorial(localNumber - 1);
+    }
+}
